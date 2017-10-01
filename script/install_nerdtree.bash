8b38ee0d68f011873cbc81d224550455e59f2903
Hiroyuki Sano <sh19910711@gmail.com>
2017-10-01 18:37:12 +0000

    remove script

--- script/install_nerdtree.bash
+++
@@ -1,6 +0,0 @@
-install_nerdtree() {
-  local bundle_dir=$(dirname ${BASH_SOURCE})/../bundle
-  git clone --depth 1 -b master --single-branch https://github.com/scrooloose/nerdtree.git "${bundle_dir}/nerdtree"
-}
-
-install_nerdtree $@
